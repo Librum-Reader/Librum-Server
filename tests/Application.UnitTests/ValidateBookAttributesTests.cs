2808c9a0d44364f6d37251f4602adb36818505a7
Creapermann <prtnprvtmail@protonmail.com>
2022-06-03 19:24:40 +0200

    Added tests for all validation filters

--- tests/Application.UnitTests/ValidateBookAttributesTests.cs
+++ tests/Application.UnitTests/ValidateBookAttributesTests.cs
@@ -20,11 +20,15 @@ namespace Application.UnitTests;
 public class ValidateBookAttributesTests
 {
     private readonly Mock<IUserRepository> _userRepositoryMock = new();
+    private readonly Mock<IBookRepository> _bookRepositoryMock = new();
     private readonly Mock<ILogger<ValidateBookExistsAttribute>> _bookExistsLoggerMock = new();
     private readonly Mock<ILogger<ValidateBookDoesNotExistAttribute>> _bookDoesNotExistLoggerMock = new();
+    private readonly Mock<ILogger<ValidateBookHasTagAttribute>> _bookHasTagLoggerMock = new();
 
     private readonly ValidateBookExistsAttribute _bookExistsFilterAttribute;
     private readonly ValidateBookDoesNotExistAttribute _bookDoesNotExistFilterAttribute;
+    private readonly ValidateBookHasTagAttribute _bookHasTagFilterAttribute;
+
 
 
     public ValidateBookAttributesTests()
@@ -34,6 +38,9 @@ public class ValidateBookAttributesTests
 
         _bookDoesNotExistFilterAttribute = new ValidateBookDoesNotExistAttribute(_userRepositoryMock.Object,
             _bookDoesNotExistLoggerMock.Object);
+        
+        _bookHasTagFilterAttribute = new ValidateBookHasTagAttribute(_userRepositoryMock.Object, 
+            _bookRepositoryMock.Object, _bookHasTagLoggerMock.Object);
     }
 
 
@@ -272,5 +279,179 @@ public class ValidateBookAttributesTests
         // Assert
         await Assert.ThrowsAsync<InternalServerException>(() => 
             _bookDoesNotExistFilterAttribute.OnActionExecutionAsync(executingContext, async () => context));
+    }
+
+
+
+
+    [Fact]
+    public async Task ValidateBookHasTag_ShouldSucceed_WhenBookHasTag()
+    {
+        // Arrange
+        const string bookTitle = "SomeBook";
+        const string tagName = "SomeTag";
+        
+        var user = new User
+        {
+            Books = new List<Book>
+            {
+                new Book
+                {
+                    Title = bookTitle,
+                    Tags = new List<Tag>
+                    {
+                        new Tag { Name = tagName }
+                    }
+                }
+            }
+        };
+
+        var modelState = new ModelStateDictionary();
+        var httpContextMock = new DefaultHttpContext();
+
+        var actionContext = new ActionContext(
+            httpContextMock,
+            Mock.Of<RouteData>(),
+            Mock.Of<ActionDescriptor>(),
+            modelState
+        );
+
+        var executingContext = new ActionExecutingContext(
+            actionContext,
+            new List<IFilterMetadata>(),
+            new Dictionary<string, object>()!,
+            modelState
+        );
+
+        executingContext.ActionArguments.Add("tagName", tagName);
+        executingContext.ActionArguments.Add("bookTitle", bookTitle);
+        
+
+        _userRepositoryMock.Setup(x => x.GetAsync(It.IsAny<string>(), It.IsAny<bool>()))
+            .ReturnsAsync(user);
+
+
+        // Act
+        var context = new ActionExecutedContext(executingContext, new List<IFilterMetadata>(), Mock.Of<Controller>());
+        await _bookHasTagFilterAttribute.OnActionExecutionAsync(executingContext, async () => context);
+
+        // Assert
+        Assert.Equal(200, executingContext.HttpContext.Response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task ValidateBookHasTag_ShouldFail_WhenBookDoesNotHaveTag()
+    {
+        // Arrange
+        const string bookTitle = "SomeBook";
+        
+        var user = new User
+        {
+            Books = new List<Book>
+            {
+                new Book
+                {
+                    Title = bookTitle,
+                    Tags = new List<Tag>
+                    {
+                        new Tag { Name = "SomeTag" }
+                    }
+                }
+            }
+        };
+
+        var modelState = new ModelStateDictionary();
+        var httpContextMock = new DefaultHttpContext();
+
+        var actionContext = new ActionContext(
+            httpContextMock,
+            Mock.Of<RouteData>(),
+            Mock.Of<ActionDescriptor>(),
+            modelState
+        );
+
+        var executingContext = new ActionExecutingContext(
+            actionContext,
+            new List<IFilterMetadata>(),
+            new Dictionary<string, object>()!,
+            modelState
+        );
+
+        executingContext.ActionArguments.Add("tagName", "SomeOtherTag");
+        executingContext.ActionArguments.Add("bookTitle", bookTitle);
+        
+
+        _userRepositoryMock.Setup(x => x.GetAsync(It.IsAny<string>(), It.IsAny<bool>()))
+            .ReturnsAsync(user);
+
+
+        // Act
+        var context = new ActionExecutedContext(executingContext, new List<IFilterMetadata>(), Mock.Of<Controller>());
+        await _bookHasTagFilterAttribute.OnActionExecutionAsync(executingContext, async () => context);
+
+        // Assert
+        Assert.Equal(400, executingContext.HttpContext.Response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task ValidateBookHasTag_ShouldThrow_WhenNoBookTitleFound()
+    {
+        // Arrange
+        var modelState = new ModelStateDictionary();
+        var httpContextMock = new DefaultHttpContext();
+
+        var actionContext = new ActionContext(
+            httpContextMock,
+            Mock.Of<RouteData>(),
+            Mock.Of<ActionDescriptor>(),
+            modelState
+        );
+
+        var executingContext = new ActionExecutingContext(
+            actionContext,
+            new List<IFilterMetadata>(),
+            new Dictionary<string, object>()!,
+            modelState
+        );
+
+        executingContext.ActionArguments.Add("tagName", "SomeTag");
+
+
+        // Act
+        var context = new ActionExecutedContext(executingContext, new List<IFilterMetadata>(), Mock.Of<Controller>());
+        await Assert.ThrowsAsync<InternalServerException>(() => 
+            _bookHasTagFilterAttribute.OnActionExecutionAsync(executingContext, async () => context));
+        
+    }
+    
+    [Fact]
+    public async Task ValidateBookHasTag_ShouldThrow_WhenNoTagNameFound()
+    {
+        // Arrange
+        var modelState = new ModelStateDictionary();
+        var httpContextMock = new DefaultHttpContext();
+
+        var actionContext = new ActionContext(
+            httpContextMock,
+            Mock.Of<RouteData>(),
+            Mock.Of<ActionDescriptor>(),
+            modelState
+        );
+
+        var executingContext = new ActionExecutingContext(
+            actionContext,
+            new List<IFilterMetadata>(),
+            new Dictionary<string, object>()!,
+            modelState
+        );
+
+        executingContext.ActionArguments.Add("bookTitle", "SomeBook");
+
+
+        // Act
+        var context = new ActionExecutedContext(executingContext, new List<IFilterMetadata>(), Mock.Of<Controller>());
+        await Assert.ThrowsAsync<InternalServerException>(() => 
+            _bookHasTagFilterAttribute.OnActionExecutionAsync(executingContext, async () => context));
+        
     }
 }
---
+++ tests/Application.UnitTests/ValidateStringParameterTests.cs
@@ -0,0 +1,150 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Common.ActionFilters;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace Application.UnitTests;
+
+public class ValidateStringParameterTests
+{
+    private readonly Mock<ILogger<ValidateStringParameterAttribute>> _loggerMock = new();
+    private readonly ValidateStringParameterAttribute _filterAttribute;
+    
+
+    public ValidateStringParameterTests()
+    {
+        _filterAttribute = new ValidateStringParameterAttribute(_loggerMock.Object);
+    }
+
+
+    [Fact]
+    public async Task ValidateStringParameter_ShouldSucceed_WhenStringIsValid()
+    {
+        var modelState = new ModelStateDictionary();
+        var httpContextMock = new DefaultHttpContext();
+
+        var actionContext = new ActionContext(
+            httpContextMock,
+            Mock.Of<RouteData>(),
+            Mock.Of<ActionDescriptor>(),
+            modelState
+        );
+
+        var executingContext = new ActionExecutingContext(
+            actionContext,
+            new List<IFilterMetadata>(),
+            new Dictionary<string, object>()!,
+            modelState
+        );
+
+        executingContext.ActionArguments.Add("SomeValidString", "this is an valid string");
+        executingContext.ActionArguments.Add("AnotherValidString", "SomePassword123");
+
+        // Act
+        var context = new ActionExecutedContext(executingContext, new List<IFilterMetadata>(), Mock.Of<Controller>());
+        await _filterAttribute.OnActionExecutionAsync(executingContext, async () => context);
+
+        // Assert
+        Assert.Equal(200, executingContext.HttpContext.Response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task ValidateStringParameter_ShouldSucceed_WhenNoStringArgumentExists()
+    {
+        var modelState = new ModelStateDictionary();
+        var httpContextMock = new DefaultHttpContext();
+
+        var actionContext = new ActionContext(
+            httpContextMock,
+            Mock.Of<RouteData>(),
+            Mock.Of<ActionDescriptor>(),
+            modelState
+        );
+
+        var executingContext = new ActionExecutingContext(
+            actionContext,
+            new List<IFilterMetadata>(),
+            new Dictionary<string, object>()!,
+            modelState
+        );
+
+        executingContext.ActionArguments.Add("SomeValidString", new User());
+        executingContext.ActionArguments.Add("AnotherValidString", 32);
+
+        // Act
+        var context = new ActionExecutedContext(executingContext, new List<IFilterMetadata>(), Mock.Of<Controller>());
+        await _filterAttribute.OnActionExecutionAsync(executingContext, async () => context);
+
+        // Assert
+        Assert.Equal(200, executingContext.HttpContext.Response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task ValidateStringParameter_ShouldFail_WhenStringIsNull()
+    {
+        var modelState = new ModelStateDictionary();
+        var httpContextMock = new DefaultHttpContext();
+
+        var actionContext = new ActionContext(
+            httpContextMock,
+            Mock.Of<RouteData>(),
+            Mock.Of<ActionDescriptor>(),
+            modelState
+        );
+
+        var executingContext = new ActionExecutingContext(
+            actionContext,
+            new List<IFilterMetadata>(),
+            new Dictionary<string, object>()!,
+            modelState
+        );
+
+        executingContext.ActionArguments.Add("SomeValidString", null);
+
+        // Act
+        var context = new ActionExecutedContext(executingContext, new List<IFilterMetadata>(), Mock.Of<Controller>());
+        await _filterAttribute.OnActionExecutionAsync(executingContext, async () => context);
+
+        // Assert
+        Assert.Equal(400, executingContext.HttpContext.Response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task ValidateStringParameter_ShouldFail_WhenStringIsWhitespace()
+    {
+        var modelState = new ModelStateDictionary();
+        var httpContextMock = new DefaultHttpContext();
+
+        var actionContext = new ActionContext(
+            httpContextMock,
+            Mock.Of<RouteData>(),
+            Mock.Of<ActionDescriptor>(),
+            modelState
+        );
+
+        var executingContext = new ActionExecutingContext(
+            actionContext,
+            new List<IFilterMetadata>(),
+            new Dictionary<string, object>()!,
+            modelState
+        );
+
+        executingContext.ActionArguments.Add("SomeValidString", "   ");
+
+        // Act
+        var context = new ActionExecutedContext(executingContext, new List<IFilterMetadata>(), Mock.Of<Controller>());
+        await _filterAttribute.OnActionExecutionAsync(executingContext, async () => context);
+
+        // Assert
+        Assert.Equal(400, executingContext.HttpContext.Response.StatusCode);
+    }
+}
