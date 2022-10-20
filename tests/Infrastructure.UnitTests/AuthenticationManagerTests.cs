7caa53f29a25b9f41123a2ecdc3b02f7b07d2130
Creapermann <prtnprvtmail@protonmail.com>
2022-10-20 13:45:06 +0200

    Fixed naming and indentation

--- tests/Infrastructure.UnitTests/AuthenticationManagerTests.cs
+++ tests/Infrastructure.UnitTests/AuthenticationManagerTests.cs
@@ -14,80 +14,77 @@ namespace Infrastructure.UnitTests;
 public class AuthenticationManagerTests
 {
     private readonly AuthenticationManager _authenticationManager;
-    private readonly Mock<IConfiguration> _configurationMock = new Mock<IConfiguration>();
-    private readonly Mock<UserManager<User>> _userManagerMock = TestHelpers.MockUserManager<User>();
+    private readonly Mock<IConfiguration> _configurationMock = new();
+    private readonly Mock<UserManager<User>> _userManagerMock =
+        TestHelpers.MockUserManager<User>();
 
 
     public AuthenticationManagerTests()
     {
-        _authenticationManager = new AuthenticationManager(_configurationMock.Object, _userManagerMock.Object);
+        _authenticationManager = new AuthenticationManager(_configurationMock.Object,
+                                                           _userManagerMock.Object);
     }
 
 
     [Fact]
-    public async Task UserExistsAsync_ShouldReturnTrue_WhenEmailAndPasswordMatch()
+    public async Task AnAuthenticationManager_SucceedsCheckingIfAUserExists()
     {
         // Arrange
         _userManagerMock.Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
             .ReturnsAsync(new User());
 
-        _userManagerMock.Setup(x => x.CheckPasswordAsync(It.IsAny<User>(), It.IsAny<string>()))
+        _userManagerMock.Setup(x => x.CheckPasswordAsync(It.IsAny<User>(),
+                                                         It.IsAny<string>()))
             .ReturnsAsync(true);
 
         // Act
-        bool result = await _authenticationManager.UserExistsAsync("JohnDoe@gmail.com", "MyPassword123");
+        var result = await _authenticationManager.UserExistsAsync("JohnDoe@gmail.com",
+                                                                  "MyPassword123");
 
         // Assert
         Assert.True(result);
     }
 
     [Fact]
-    public async Task UserExistsAsync_ShouldReturnFalse_WhenEmailDoesNotExist()
+    public async Task AnAuthenticationManager_FailsCheckingIfAUserExistsIfEmailIsWrong()
     {
         // Arrange
         _userManagerMock.Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
             .ReturnsAsync(() => null!);
 
-        _userManagerMock.Setup(x => x.CheckPasswordAsync(It.IsAny<User>(), It.IsAny<string>()))
+        _userManagerMock.Setup(x => x.CheckPasswordAsync(It.IsAny<User>(),
+                                                         It.IsAny<string>()))
             .ReturnsAsync(true);
 
         // Act
-        bool result = await _authenticationManager.UserExistsAsync("JohnDoe@gmail.com", "MyPassword123");
+        var result = await _authenticationManager.UserExistsAsync("JohnDoe@gmail.com",
+                                                                  "MyPassword123");
 
         // Assert
         Assert.False(result);
     }
     
     [Fact]
-    public async Task UserExistsAsync_ShouldReturnFalse_WhenPasswordDoesNotMatch()
+    public async Task AnAuthenticationManager_FailsCheckingIfAUserExistsIfPasswordIsWrong()
     {
         // Arrange
         _userManagerMock.Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
             .ReturnsAsync(new User());
 
-        _userManagerMock.Setup(x => x.CheckPasswordAsync(It.IsAny<User>(), It.IsAny<string>()))
+        _userManagerMock.Setup(x => x.CheckPasswordAsync(It.IsAny<User>(),
+                                                         It.IsAny<string>()))
             .ReturnsAsync(false);
 
         // Act
-        bool result = await _authenticationManager.UserExistsAsync("JohnDoe@gmail.com", "MyPassword123");
+        var result = await _authenticationManager.UserExistsAsync("JohnDoe@gmail.com",
+                                                                  "MyPassword123");
 
         // Assert
         Assert.False(result);
     }
-
-    [Fact]
-    public async Task CreateTokenAsync_ShouldThrow_WhenUserDoesNotExist()
-    {
-        // Arrange
-        _configurationMock.Setup(p => p[It.IsAny<string>()])
-            .Returns("SomeString12345");
-        
-        // Assert
-        await Assert.ThrowsAsync<ArgumentException>(() => _authenticationManager.CreateTokenAsync(null));
-    }
     
     [Fact]
-    public async Task CreateTokenAsync_ShouldReturnToken_WhenUserExists()
+    public async Task AnAuthenticationManager_SucceedsCreatingToken()
     {
         // Arrange
         var user = new User
@@ -106,7 +103,7 @@ public class AuthenticationManagerTests
         };
         
         _configurationMock.Setup(x => x[It.IsAny<string>()])
-            .Returns("SomeLongongongongongongongongongongongongString12345");
+            .Returns("SomeLoooooooooooooooooooooooooooooooooongString12345");
 
         _userManagerMock.Setup(x => x.FindByEmailAsync(It.IsNotNull<string>()))
             .ReturnsAsync(user);
@@ -120,5 +117,17 @@ public class AuthenticationManagerTests
 
         // Assert
         Assert.NotEmpty(result);
+    }
+
+    [Fact]
+    public async Task AnAuthenticationManager_FailsCreatingTokenIfUserDoesNotExist()
+    {
+        // Arrange
+        _configurationMock.Setup(p => p[It.IsAny<string>()])
+            .Returns("SomeString12345");
+        
+        // Assert
+        await Assert.ThrowsAsync<ArgumentException>(
+            () => _authenticationManager.CreateTokenAsync(null));
     }
 }
