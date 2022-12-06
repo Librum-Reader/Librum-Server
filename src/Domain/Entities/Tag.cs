490baf88b375b79f5461f816a1ec3bbf2be5ab6a
Creapermann <prtnprvtmail@protonmail.com>
2022-12-06 21:50:18 +0100

    Disabled automatic Guid generation

--- src/Domain/Entities/Tag.cs
+++ src/Domain/Entities/Tag.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
 namespace Domain.Entities;
@@ -6,6 +7,7 @@ namespace Domain.Entities;
 [Index(nameof(Name), IsUnique = true)]
 public class Tag
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
     [Key]
     public Guid TagId { get; set; }
     
