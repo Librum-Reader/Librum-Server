c467e14485cb3710c2035385e72ebe6a7863937a
Creapermann <prtnprvtmail@protonmail.com>
2022-12-06 21:56:13 +0100

    Added Guid

--- src/Application/Common/DTOs/Tags/TagInDto.cs
+++ src/Application/Common/DTOs/Tags/TagInDto.cs
@@ -5,6 +5,9 @@ namespace Application.Common.DTOs.Tags;
 public class TagInDto
 {
     [Required]
+    public string Guid { get; set; }
+    
+    [Required]
     [MinLength(2, ErrorMessage = "The tag name is too short")]
     [MaxLength(30, ErrorMessage = "The tag name is too long")]
     public string Name { get; set; }
