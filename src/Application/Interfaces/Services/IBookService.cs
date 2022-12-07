8dec4742683bb3be8241eb86a9ebdfef51180e4a
Creapermann <prtnprvtmail@protonmail.com>
2022-12-07 17:51:54 +0100

    Changed parameters of tag manipulating methods

--- src/Application/Interfaces/Services/IBookService.cs
+++ src/Application/Interfaces/Services/IBookService.cs
@@ -1,4 +1,5 @@
 using Application.Common.DTOs.Books;
+using Application.Common.DTOs.Tags;
 
 namespace Application.Interfaces.Services;
 
@@ -6,9 +7,8 @@ public interface IBookService
 {
     Task CreateBookAsync(string email, BookInDto bookInDto, string guid);
     Task<IList<BookOutDto>> GetBooksAsync(string email);
-    Task AddTagsToBookAsync(string email, string bookGuid,
-                            IEnumerable<string> tagNames);
-    Task RemoveTagFromBookAsync(string email, string bookGuid, string tagName);
+    Task AddTagsToBookAsync(string email, string bookGuid, TagInDto tag);
+    Task RemoveTagFromBookAsync(string email, string bookGuid, string tagGuid);
     Task DeleteBooksAsync(string email, IEnumerable<string> bookGuids);
     Task PatchBookAsync(string email, BookForUpdateDto bookUpdateDto,
                         string bookGuid);
