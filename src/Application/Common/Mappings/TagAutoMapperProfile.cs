0fd4f0e8f2a1556a7c4736eed54f4f3e6a045776
Creapermann <prtnprvtmail@protonmail.com>
2022-12-10 13:21:35 +0100

    Added vertical spacing

--- src/Application/Common/Mappings/TagAutoMapperProfile.cs
+++ src/Application/Common/Mappings/TagAutoMapperProfile.cs
@@ -11,6 +11,7 @@ public class TagAutoMapperProfile : Profile
         CreateMap<TagInDto, Tag>()
             .ForMember(dest => dest.CreationDate, temp => temp.MapFrom(src => DateTime.UtcNow))
             .ForMember(dest => dest.TagId, temp => temp.MapFrom(src => new Guid(src.Guid)));
+        
         CreateMap<Tag, TagOutDto>()
             .ForMember(dest => dest.Guid, temp => temp.MapFrom(src => src.TagId.ToString()));
     }
