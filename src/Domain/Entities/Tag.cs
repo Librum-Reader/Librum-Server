45c77df79a85d6c1b7d826b229a593d8c49e98f3
DavidLazarescu <prtnprvtmail@protonmail.com>
2023-02-11 22:50:18 +0100

    Removed incorrect unique constraint on tag names

--- src/Domain/Entities/Tag.cs
+++ src/Domain/Entities/Tag.cs
@@ -1,10 +1,8 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace Domain.Entities;
 
-[Index(nameof(Name), IsUnique = true)]
 public class Tag
 {
     [DatabaseGenerated(DatabaseGeneratedOption.None)]
