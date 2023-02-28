9dcf9b2d20295c997f9c2c6e3f8b9342c20ab6d4
DavidLazarescu <prtnprvtmail@protonmail.com>
2023-02-28 15:40:31 +0100

    Removed seeding

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -26,11 +26,6 @@ using (var scope = app.Services.CreateScope())
 
     var loggerFactory = services.GetRequiredService<ILoggerFactory>();
     loggerFactory.AddFile(Directory.GetCurrentDirectory() + "/Data/Logs/");
-
-    await DataContextSeeding.SeedDataContext(
-        services.GetRequiredService<DataContext>(),
-        services.GetRequiredService<IAuthenticationService>(),
-        services.GetRequiredService<IBookService>());
 }
 
 
