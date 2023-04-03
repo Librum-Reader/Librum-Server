5aef35fbed4db674e31d1f36732a28348b83d452
DavidLazarescu <prtnprvtmail@protonmail.com>
2023-04-03 18:27:21 +0200

    Moved AuthenticationManager

--- src/Application/Interfaces/Managers/IAuthenticationManager.cs
+++ src/Application/Interfaces/Managers/IAuthenticationManager.cs
@@ -1,7 +1,7 @@
 using Application.Common.DTOs.Users;
 using Domain.Entities;
 
-namespace Application.Interfaces;
+namespace Application.Interfaces.Managers;
 
 public interface IAuthenticationManager
 {
