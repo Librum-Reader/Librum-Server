ead27b612d5e17d08f9d43703bfc246869453371
DavidLazarescu <prtnprvtmail@protonmail.com>
2023-04-03 18:30:51 +0200

    Added book uploading in controller

--- src/Presentation/Controllers/v1/BookController.cs
+++ src/Presentation/Controllers/v1/BookController.cs
@@ -1,7 +1,8 @@
-using Application.Common.ActionFilters;
+using Application.Common.Attributes;
 using Application.Common.DTOs.Books;
 using Application.Common.Exceptions;
 using Application.Interfaces.Services;
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
@@ -18,31 +19,48 @@ public class BookController : ControllerBase
 {
     private readonly ILogger<BookController> _logger;
     private readonly IBookService _bookService;
+    private readonly BlobServiceClient _blobServiceClient;
 
-
-    public BookController(ILogger<BookController> logger, IBookService bookService)
+    
+    public BookController(ILogger<BookController> logger, IBookService bookService,
+                          BlobServiceClient blobServiceClient)
     {
         _logger = logger;
         _bookService = bookService;
+        _blobServiceClient = blobServiceClient;
     }
 
-    [HttpPost("data")]
-    public async Task<ActionResult> UploadData()
+    /// Books are created in two steps, first of all an entry containing all of their
+    /// meta data is created on the Database (See 'CreateBook()') and then their
+    /// binary data, so their actual content is added to the existing book.
+    [HttpPost("addBookBinaryData/{guid:guid}")]
+    [DisableFormValueModelBinding]
+    public async Task<ActionResult> AddBookBinaryData(Guid guid)
     {
-        Console.WriteLine("In!");
+        // Check if the book binary data was sent in the correct format
         var isMultiPart = !string.IsNullOrEmpty(Request.ContentType) &&
                           Request.ContentType.IndexOf(
                               "multipart/",
                               StringComparison.OrdinalIgnoreCase) >= 0;
         if (!isMultiPart)
         {
-            ModelState.AddModelError("File",
-                                     $"The request couldn't be processed (Error 1).");
-            return BadRequest(ModelState);
+            var message = "The book binary data needs to be sent as multipart";
+            return BadRequest(message);
         }
         
         var boundary = GetBoundary(MediaTypeHeaderValue.Parse(Request.ContentType));
         var reader = new MultipartReader(boundary, HttpContext.Request.Body);
+        
+        // await _bookService.AddBookBinaryData(HttpContext.User.Identity!.Name, 
+        //                                      guid,
+        //                                      reader);
+        
+        var containerClient =
+            _blobServiceClient.GetBlobContainerClient("librumdev");
+        var blobClient = containerClient.GetBlobClient(guid.ToString());
+
+        await using var dest = await blobClient.OpenWriteAsync(true);
+
         
         var section = await reader.ReadNextSectionAsync();
         while (section != null)
@@ -57,20 +75,28 @@ public class BookController : ControllerBase
         
             if (!HasFileContentDisposition(contentDisposition))
             {
-                ModelState.AddModelError("File",
-                                         $"The request couldn't be processed (Error 2).");
-        
-                return BadRequest(ModelState);
+                var message = "Missing content disposition header";
+                throw new InvalidParameterException(message);
             }
-        
-            Console.WriteLine(await section.ReadAsStringAsync());
+            
+            await section.Body.CopyToAsync(dest);
             
             section = await reader.ReadNextSectionAsync();
         }
-
+        
         return Ok();
     }
-
+    
+    private static bool HasFileContentDisposition(
+        ContentDispositionHeaderValue contentDisposition)
+    {
+        // Content-Disposition: form-data; name="myfile1"; filename="Misc 002.jpg"
+        return contentDisposition != null &&
+               contentDisposition.DispositionType.Equals("form-data") &&
+               (!string.IsNullOrEmpty(contentDisposition.FileName.Value) ||
+                !string.IsNullOrEmpty(contentDisposition.FileNameStar.Value));
+    }
+    
     private static string GetBoundary(MediaTypeHeaderValue contentType)
     {
         var boundary = HeaderUtilities.RemoveQuotes(contentType.Boundary).Value;
@@ -81,16 +107,6 @@ public class BookController : ControllerBase
         }
 
         return boundary;
-    }
-
-    private static bool HasFileContentDisposition(
-        ContentDispositionHeaderValue contentDisposition)
-    {
-        // Content-Disposition: form-data; name="myfile1"; filename="Misc 002.jpg"
-        return contentDisposition != null &&
-               contentDisposition.DispositionType.Equals("form-data") &&
-               (!string.IsNullOrEmpty(contentDisposition.FileName.Value) ||
-                !string.IsNullOrEmpty(contentDisposition.FileNameStar.Value));
     }
 
     [HttpPost]
