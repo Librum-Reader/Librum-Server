75e2be66c7674270cef1b7f862fd1b2e90b46a05
DavidLazarescu <prtnprvtmail@protonmail.com>
2023-07-31 09:13:12 +0200

    Enabled sensitive logging

--- src/Presentation/DependencyInjection.cs
+++ src/Presentation/DependencyInjection.cs
@@ -69,7 +69,8 @@ public static class DependencyInjection
             {
                 throw new InvalidDataException("Failed getting the SQL connection string");
             }
-            
+
+            options.EnableSensitiveDataLogging(true);
             options.UseSqlServer(connectionString);
         });
         
