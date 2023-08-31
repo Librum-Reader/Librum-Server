59a3b178f25be098b28959ec18dc1f465e7a9623
DavidLazarescu <prtnprvtmail@protonmail.com>
2023-08-31 15:57:42 +0200

    Allow Anonymous for AppInfo endpoint

--- src/Presentation/Controllers/AppInfoController.cs
+++ src/Presentation/Controllers/AppInfoController.cs
@@ -25,7 +25,7 @@ public class AppInfoController : ControllerBase
         _configuration = configuration;
     }
 
-
+    [AllowAnonymous]
     [HttpGet("latest-version")]
     public async Task<string> GetLatestVersion()
     {
